5756f6768134162a36dbfc79115d5addcda66c85
Felix H. Dahlke <fhd@ubercode.de>
2010-09-14 15:58:55 +0200

    Added Javadoc.

--- src/main/java/com/github/fhd/gwtetris/client/GWTetris.java
+++ src/main/java/com/github/fhd/gwtetris/client/GWTetris.java
@@ -3,7 +3,13 @@ package com.github.fhd.gwtetris.client;
 import com.google.gwt.core.client.*;
 import com.google.gwt.user.client.ui.*;
 
+/**
+ * The entry point of GWTetris.
+ */
 public class GWTetris implements EntryPoint {
+    /**
+     * The entry point method.
+     */
     public void onModuleLoad() {
         RootPanel.get().add(new Label("Hello, World!"));
     }
