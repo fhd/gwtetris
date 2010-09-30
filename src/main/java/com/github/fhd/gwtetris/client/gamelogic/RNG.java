2c9c42c745aabea672c7a187b4d8c00eee04e328
Felix H. Dahlke <fhd@ubercode.de>
2010-09-30 20:53:03 +0200

    Comment refinement.

--- src/main/java/com/github/fhd/gwtetris/client/gamelogic/RNG.java
+++ src/main/java/com/github/fhd/gwtetris/client/gamelogic/RNG.java
@@ -5,14 +5,14 @@ package com.github.fhd.gwtetris.client.gamelogic;
  */
 public interface RNG {
     /**
-     * Creates a random typeNo identifying the type of a piece.
+     * Returns a random typeNo identifying the type of a piece.
      * @param numTypes The number of available types.
      * @return A random number between 0 and the maximum possible typeNo.
      */
     int randomPieceTypeNo(int numTypes);
 
     /**
-     * Creates a random position for a piece. Works for both x and y
+     * Returns a random position for a piece. Works for both x and y
      * coordinates.
      * @param maxPos The maximum possible position.
      * @return A random x or y coordinate for a piece.
