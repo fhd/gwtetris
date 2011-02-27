1c40ef49b390b2b6ebeb3572a017d6dca8c1ffd7
Felix H. Dahlke <fhd@ubercode.de>
2011-02-27 08:55:12 +0100

    Reduced grid size.

--- src/main/java/com/github/fhd/gwtetris/client/Constants.java
+++ src/main/java/com/github/fhd/gwtetris/client/Constants.java
@@ -7,8 +7,8 @@ class Constants {
     static final int STEP_TIME = 300;
     static final int BLOCK_HEIGHT = 20;
     static final int BLOCK_WIDTH = 20;
-    static final int GRID_ROWS = 32;
-    static final int GRID_COLS = 20;
+    static final int GRID_ROWS = 20;
+    static final int GRID_COLS = 10;
 
     private Constants() {
     }
